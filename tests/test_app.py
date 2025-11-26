198dc1df22f1b1d2a735e0e9f624287b868f39d9
matiasalarcon6 <mati.gb.6@gmail.com>
2025-11-26 00:06:40 -0300

    Fix import path for GitHub Actions

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,3 +3,4 @@ from app.app import app
 def test_home():
     response = app.test_client().get("/")
     assert response.status_code == 200
+    assert b"Microservicio Python funcionando" in response.data
